065c669a54e885609bc220243ff0df388128fcb1
jatinbhalla <jatinbhalla18.jb@gmail.com>
2026-06-02 13:08:30 +0530

    backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-1991"
+    key    = "devjune2026.tfstate"
+    region = "us-east-1"
+  }
+}
+
